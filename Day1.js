08b5bb2ac372296e95f4c7a816b36af047713eca
AGowthamSaiBabu <allamgowthamsaibabu@gmail.com>
2026-01-10 12:16:13 +0530

    variables

---
+++ Day1.js
@@ -0,0 +1,81 @@
+// Topic : Variables
+// 1.What is the value of x after the code executes?
+var x;
+x=5;
+var x=10;
+console.log(x);//10
+
+//2.What is the output of the following code?
+console.log(x1);//undefined
+var x1=15;
+
+//3.How many times is the variable y redeclared in this code?
+var y=1;
+vary =2;
+y=3;
+console.log(y);//3
+
+//4.What happens when you declare a var variable without initializing it?
+var b;
+console.log(b);//undefined
+
+//5. What will the following code print?
+var a=1;
+{
+    var a=2;
+}
+console.log(a);//2
+
+//6.What happens when you declare a var variable inside a block and access n it outside?
+{
+    var c=10;
+}
+console.log(c);//10
+
+//7.What is the output of the following code?
+var p;
+console.log(p);//undefined
+p=2;
+
+//8.What happens if you declare a var variable and later redeclare it in the same scope?
+var q=5;
+var q=10;
+console.log(q);//10
+
+//9.What is the value of r after this code executes?
+var r;
+r=30;
+var r =50;
+console.log(r);//50
+
+//10.What will this code output?
+console.log(x3);//undefined
+var x3 = 25;
+
+//11.What is the result of this code?
+var_alphas$=50;
+var_alphas$=100;
+console.log(var_alphas$);//100
+
+//12.What does this print?
+var $$$ =99;
+$$$ =88;
+console.log($$$);//88
+
+
+// 13.What is the final value of 12x$var_?
+// var 12x$var_=100;
+// console.log(12x$var);//Error
+
+// 14.What happens when you declare a var variable twice in
+//  a global scope?
+var y =5;
+var y =10;
+console.log(y);//10
+
+// 15. What is the output of this code?
+var z =1;
+z=2;
+console.log(z);//2
+
+
