c1f6c6523b04653aeaac95542a4b23286542111e
AGowthamSaiBabu <allamgowthamsaibabu@gmail.com>
2026-08-30 12:43:47 +0530

    Arrays

---
+++ Day19.js
@@ -0,0 +1,71 @@
+// 1)Add elements to an array and iterate using for loop and for in loop and for of loop
+// Create an array of your favorite movies and iterate an array to the console.
+
+let movie =["OG","Kalki","pushpa","salar"];
+
+console.log("Using for loop:");
+for(let i=0;i<movie.length;i++){
+  console.log(movie[i]);
+}
+
+console.log("Using for...in loop");
+for(let index in movie){
+  console.log(movie[index]);
+  
+}
+
+console.log("Using for...of loop");
+for(let value of movie){
+  console.log(value);
+  
+}
+
+// 2)Remove elements from an array
+// Remove the first and last elements from the array.
+let arr =[10,20,30,40,50];
+
+arr.shift();
+arr.pop();
+
+console.log(arr);
+
+// 3)Reverse an array using for loop
+// hints
+// use push method
+let num =[10,20,30,40];
+let Reverse =[]
+for(let i=num.length-1;i>=0;i--){
+  Reverse.push(num[i])
+}
+console.log(Reverse);
+
+// 4) find the even and odd numbers in an array [12,3,5,6,22,56,29]
+// and print the evensum and oddsum
+
+let arr1 =[12,3,5,6,22,56,29];
+let EvenSum=0;
+let OddSum=0;
+
+for(let i=0;i<arr1.length;i++){
+  if( arr1[i]%2==0){
+     console.log(arr[i]);
+     EvenSum =EvenSum+arr1[i];
+  }
+}
+
+for(let i=0;i<arr.length;i++){
+  if(arr1[i]%2==1){
+    console.log(arr1[i]);
+    OddSum =OddSum+arr1[i];
+  }
+}
+
+console.log(EvenSum);
+console.log(OddSum);
+
+
+
+
+
+
+
